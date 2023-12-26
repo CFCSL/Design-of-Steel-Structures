9147f747b269184733ff633549067275532002a3
NamNguyen2015 <111955400+NamNguyen2015@users.noreply.github.com>
2023-12-26 13:52:48 +0100

    update str

--- Patch_Loading.py
+++ Patch_Loading.py
@@ -123,8 +123,8 @@ import matplotlib.pyplot as plt
 # =============================================================================
 
 #%%
-F_Ed, t_w, h_w, b_f, f_yf, t_f, f_yw, gamma_M1, E, S_S, a, c, epsilon, f_y = symbols(
-	'F_Ed t_w h_w b_f f_yf t_f f_yw gamma_M1 E S_S a c epsilon f_y')
+F_Ed, t_w, h_w, b_f, f_yf, t_f, f_yw, gamma_M1, E, s_s, a, c, epsilon, f_y = symbols(
+	'F_Ed t_w h_w b_f f_yf t_f f_yw gamma_M1 E s_s a c epsilon f_y')
 
 Type=str
 
@@ -137,23 +137,26 @@ def k_F_func(Type,**kwargs):
 	if Type == "B":
 		expr = 3.5 + 2 * (h_w / a)**2
 	if Type == "C":
-		expr = 2 + 6 * ((S_S + c) / h_w)
+		expr = 6 + 2 * ((s_s + c) / h_w)**2
+	expr=expr.subs(kwargs)
 	_eq=Eq(k_F,expr)
-	_eq=_eq.subs(kwargs)
+	#_eq=_eq.subs(kwargs)
 	return _eq
 
 def F_cr_func(Type, **kwargs):
 	kwargs = {eval(key): UnevaluatedExpr(value) for key, value in kwargs.items()}
 	expr=0.9*(k_F*E)*(t_w**3/h_w )
+	expr=expr.subs(kwargs)
 	_eq=Eq(F_cr,expr)
-	_eq=_eq.subs(kwargs)
+	#_eq=_eq.subs(kwargs)
 	return _eq
 	
 def m_1_func(Type, **kwargs):
 	kwargs = {eval(key): UnevaluatedExpr(value) for key, value in kwargs.items()}
 	expr= f_yf*b_f/(f_yw*t_w)
+	expr=expr.subs(kwargs)
 	_eq=Eq(m_1,expr)
-	_eq=_eq.subs(kwargs)
+	#_eq=_eq.subs(kwargs)
 	return _eq
 
 def m_2_func(Type, **kwargs):
@@ -162,233 +165,78 @@ def m_2_func(Type, **kwargs):
 	#funclist=[0.02*(h_w/t_f)**2, 0]
 	#expr = Piecewise(*zip(funclist,condlist))
 	expr=0.02*(h_w/t_f)**2
+	expr=expr.subs(kwargs)
 	_eq=Eq(m_2,expr)
-	_eq=_eq.subs(kwargs)
+	#_eq=_eq.subs(kwargs)
 	return _eq
 
-def l_e_func(Type, **kwargs):
+#l_y_func(Type, **kwargs)
+
+
+def l_e_func(Type="C", **kwargs):
 	kwargs = {eval(key): UnevaluatedExpr(value) for key, value in kwargs.items()}
-	expr=(k_F*E*t_w**2)/(2*f_yw*h_w)
+	#if Type=="C":
+	expr=(k_F*E*t_w**2)/(2*f_yw*h_w)  
+	expr=expr.subs(kwargs)
 	_eq=Eq(l_e,expr)
-	_eq=_eq.subs(kwargs)
+	#_eq=_eq.subs(kwargs)
 	return _eq
+
+
 
 
 def l_y_func(Type, **kwargs):
 	#kwargs = {eval(key): UnevaluatedExpr(value) for key, value in kwargs.items()}
 	if Type=="A" or Type=="B":
-		expr=S_S+2*t_f*(1+sqrt(m_1+m_2))
-
+		expr=s_s+2*t_f*(1+sqrt(m_1+m_2))
 	if Type=="C":
 		expr1=l_e+t_f*sqrt(m_1/2+(l_e/t_f)**2+m_2)
 		expr2=l_e+t_f*sqrt(m_1+m_2)
 		expr=Min(expr1,expr2)
+	expr=expr.subs(kwargs)
 	_eq=Eq(l_y,expr)
-	_eq=_eq.subs(kwargs)
+	#_eq=_eq.subs(kwargs)#.subs(l_e, l_e_expr)
 	return _eq
 
 def F_y_func(Type, **kwargs):
 	kwargs = {eval(key): UnevaluatedExpr(value) for key, value in kwargs.items()}
 	expr=f_yw*t_w*l_y
+	expr=expr.subs(kwargs)
 	_eq=Eq(F_y,expr)
-	_eq=_eq.subs(kwargs)
+	#_eq=_eq.subs(kwargs)
 	return _eq
 
 def lambda_F_func(Type, **kwargs):
 	kwargs = {eval(key): UnevaluatedExpr(value) for key, value in kwargs.items()}
-	expr= sqrt((F_y)/(F_cr))
+	expr= sqrt(l_y*t_w*f_yw/F_cr)
+	expr=expr.subs(kwargs)
 	_eq=Eq(lambda_F,expr)
-	_eq=_eq.subs(kwargs)
+	
 	return _eq
 
 def chi_F_func(Type,**kwargs):
 	kwargs = {eval(key): UnevaluatedExpr(value) for key, value in kwargs.items()}
 	expr= 0.5/ lambda_F
+	expr=expr.subs(kwargs)
 	_eq=Eq(chi_F,expr)
-	_eq=_eq.subs(kwargs)
+	#_eq=_eq.subs(kwargs)
 	return _eq
 
 def F_Rd_func(Type,**kwargs):
 	kwargs = {eval(key): UnevaluatedExpr(value) for key, value in kwargs.items()}
 	expr=chi_F*F_y/gamma_M1
+	expr=expr.subs(kwargs)
 	_eq=Eq(F_Rd,expr)
-	_eq=_eq.subs(kwargs)
+	#_eq=_eq.subs(kwargs)
 	return _eq
-
+	
 def eta_2_func(Type,**kwargs):
 	kwargs = {eval(key): UnevaluatedExpr(value) for key, value in kwargs.items()}
 	expr= F_Ed/F_Rd
+	expr=expr.subs(kwargs)
 	_eq=Eq(eta_2,expr)
-	_eq=_eq.subs(kwargs)
+	#_eq=_eq.subs(kwargs)
 	return _eq
-
-# =============================================================================
-# import numpy as np
-# from sympy import symbols, sqrt, UnevaluatedExpr
-# 
-# ue = UnevaluatedExpr
-# 
-# F_Ed, t_w, h_w, b_f, f_yf, t_f, f_yw, gamma_M1, E, S_S, a, c, epsilon, f_y = symbols(
-#	 'F_Ed t_w h_w b_f f_yf t_f f_yw gamma_M1 E S_S a c epsilon f_y'
-# )
-# 
-# def Patch_Loading(Type, **kwargs):
-#	 if kwargs['S_S'] > kwargs['a']:
-#		 print('FIN! La estructura no cumple frente a patch loading')
-#		 return None
-# 
-#	 k_F = 6 + 2 * ((kwargs['S_S'] + kwargs['c']) / kwargs['h_w'])**2 if Type == "C" else 6 + 2 * (kwargs['h_w'] / kwargs['a'])**2
-# 
-#	 F_cr = 0.9 * k_F * kwargs['E'] * kwargs['t_w']**3 / kwargs['h_w']
-# 
-#	 if F_cr < kwargs['F_Ed']:
-#		 print('FIN! La estructura no cumple frente a patch loading')
-#		 return None
-# 
-#	 m1 = (kwargs['f_yf'] * kwargs['b_f']) / (kwargs['f_yw'] * kwargs['t_w'])
-#	 m2 = 0.02 * (kwargs['h_w'] / kwargs['t_f'])**2
-# 
-#	 while True:
-#		 if Type == "A" or Type == "B":
-#			 l_y = kwargs['S_S'] + 2 * kwargs['t_f'] * (1 + np.sqrt(m1 + m2))
-#		 else:
-#			 l_e = (k_F * kwargs['E'] * kwargs['t_w']**2) / (2 * kwargs['f_yw'] * kwargs['h_w'])
-#			 if l_e <= kwargs['S_S'] + kwargs['c']:
-#				 l_y = min(l_e + kwargs['t_f'] * np.sqrt(m1 / 2 + (l_e / kwargs['t_f'])**2 + m2),
-#						 l_e + kwargs['t_f'] * np.sqrt(m1 + m2))
-# 
-#		 F_y = kwargs['f_yw'] * kwargs['t_w'] * l_y
-# 
-#		 if F_y < kwargs['F_Ed']:
-#			 print('FIN! La estructura no cumple frente a patch loading')
-#			 return None
-# 
-#		 lambda_F = sqrt((l_y * kwargs['t_w'] * kwargs['f_yw']) / F_cr)
-# 
-#		 if lambda_F <= 0.5:
-#			 m2 = 0
-#			 continue
-# 
-#		 Xi_r = 0.5 / lambda_F
-#		 F_Rd = Xi_r * F_y / kwargs['gamma_M1']
-# 
-#		 if F_Rd < kwargs['F_Ed']:
-#			 print('FIN! La estructura no cumple frente a patch loading')
-#			 return None
-# 
-#		 eta_2 = kwargs['F_Ed'] / F_Rd
-#		 return eta_2
-# 
-# 
-# # Update variable names in the dictionary
-# db = {'F_Ed': 3556.6, 't_w': 17.5e-3, 'h_w': 734e-4, 'b_f': 300e-3,
-#	   'f_yf': 355e3, 't_f': 33e-4, 'f_yw': 355e3, 'gamma_M1': 1.1,
-#	   'E': 200e6, 'S_S': 0.33, 'a': 0.125, 'c': 0.33,
-#	   'epsilon': 0.8136, 'f_y': 355}
-# 
-# eta2 = Patch_Loading(Type="A", **db)
-# 
-# print(eta2)
-# =============================================================================
-# =============================================================================
-# 
-# ue=UnevaluatedExpr
-# 
-# F_Ed=symbols('F_Ed')
-# t_w=symbols('t_w')
-# h_w=symbols('h_w')
-# b_f=symbols('b_f')
-# f_yf=symbols('f_yf')
-# t_f=symbols('t_f')
-# f_yw=symbols('f_yw')
-# gamma_M1=symbols('gamma_M1')
-# E=symbols('E')
-# S_S=symbols('S_S')
-# a=symbols('a')
-# c=symbols('c')
-# epsilon=symbols('epsilon')
-# f_y=symbols('f_y')
-# 
-# 
-# 
-# def Patch_Loading( Type, **kwargs):
-# 	#kwargs = {eval(key): UnevaluatedExpr(value) for key, value in kwargs.items()}
-# 
-# 	if S_S <=a:
-# 		if Type=="A":
-# 			k_F=6+2*(h_w/a)**2
-# 		if Type=="B":
-# 			k_F=3.5+2*(h_w/a)**2
-# 				
-# 		if Type=="C":
-# 			k_F=6+2*((S_S+c)/h_w)**2
-# 	
-# 	
-# 		F_cr=0.9*k_F*E*t_w**3/h_w
-# 		
-# 		if F_cr>=F_Ed:
-# 			m1= (f_yf*b_f)/(f_yw*t_w)
-# 			
-# 			# Initial values for lambda_F and m2:
-# 				
-# 			m2=0.02*(h_w/t_f)**2
-# 			lambda_F=0.1
-# 			while lambda_F<=0.5:
-# 				
-# 				if Type=="A" or "B":
-# 					l_y=S_S+2*t_f*(1+np.sqrt(m1+m2))
-# 					if l_y	<=a:
-# 						F_y=f_yw*t_w*l_y
-# 	
-# 				else:
-# 					l_e=(k_F*E*t_w**2)/(2*f_yw*h_w)
-# 					if l_e<= S_S+c:
-# 						l_y=min(l_e+t_f*np.sqrt(m1/2+(l_e/t_f)**2+m2), l_e+t_f*np.sqrt(m1+m2))
-# 						F_y=f_yw*t_w*l_y
-# 					
-# 				
-# 					
-# 				if F_y>=F_Ed:
-# 					lambda_F=np.sqrt((l_y*t_w*f_yw)/F_cr)
-# 					m2=0
-# 			
-# 			Xi_r=0.5/lambda_F
-# 			if Xi_r<=1:
-# 				F_Rd=Xi_r*F_y/gamma_M1
-# 				if F_Rd>=F_Ed:
-# 					eta_2=F_Ed/ F_Rd
-# 					
-# 					
-# 	else: 
-# 		print('FIN! La estructura no cumple frente a patch loading')
-# 		
-# 	return eta_2
-# 		
-# F_Ed_val=3556.6
-# t_w_val=17.5e-3
-# h_w_val=734e-4
-# b_f_val=300e-3
-# f_yf_val=355e3
-# t_f_val=33e-4
-# f_yw_val=355e3
-# gamma_M1_val=1.1
-# E_val=200e6
-# S_S_val=0.33
-# a_val=0.125
-# c_val=0.33
-# epsilon_val=0.8136
-# f_y_val=355
-# 
-# 
-# 
-# db={'F_Ed':F_Ed_val,'t_w':t_w_val, 'h_w':h_w_val,'b_f':b_f_val, 
-# 		'f_yf': f_yf_val,'t_f':t_f_val,'f_yw':f_yw_val,'gamma_M1':gamma_M1_val, 'E': E_val, 'S_S': S_S_val, 'a': a_val,
-# 		 'c': c_val,'epsilon': epsilon_val,'f_y': f_y_val}
-# 
-# eta2=Patch_Loading( Type="A", **db)
-# 
-# print(eta2)
-# =============================================================================
 
 
 
--- st_Patch_Loading.py
+++ st_Patch_Loading.py
@@ -71,7 +71,7 @@ gamma_M1_val = st.sidebar.number_input('Design transverse force $\\gamma_{M1} $'
 
 E_val = st.sidebar.number_input('Elasticity module $E [kN/m^2]$', value= 200.0e6, min_value=1.0e6, step=1.0e6, format="%.1f")
 
-S_S_val = st.sidebar.number_input(' $S_S [m]$', value= 300.0e-3, min_value=0.0, step=0.10, format="%.3f")
+s_s_val = st.sidebar.number_input(' $s_s [m]$', value= 300.0e-3, min_value=0.0, step=0.10, format="%.3f")
 
 a_val = st.sidebar.number_input('length of a stiffened or unstiffened plate $a [m]$', value= 1185.0e-3, min_value=0.0, step=0.10, format="%.3f")
 
@@ -79,22 +79,21 @@ c_val = st.sidebar.number_input('Distance $c [m]$', value= 900e-3, min_value=0.0
 
 db={'F_Ed':F_Ed_val, 't_w': t_w_val, 'h_w': h_w_val, 'b_f':b_f_val,
 	"f_yf": f_yf_val, "t_f":t_f_val, "f_yw":f_yw_val, "gamma_M1": gamma_M1_val,
-	"E":E_val, "S_S":S_S_val, "a":a_val, "c":c_val}
+	"E":E_val, "s_s":s_s_val, "a":a_val, "c":c_val}
 
 
 Types=["A","B","C"]
 
 Type= st.selectbox("chose type of load applicatrion", options=Types)
 
-if db['S_S'] < db['a']:
+if db['s_s'] < db['a']:
 	st.write("The interaction of the transverse force, bending moment and axial force should be verified using 7.2.")
-
 	st.latex(latex(k_F_func(Type)))
-	
 	st.latex(latex(k_F_func(Type, **db)))
 	k_F_val=N(k_F_func(Type, **db). doit(),4)
 	st.latex(latex(k_F_val))
 	db['k_F']=k_F_val.rhs
+	
 	st.write('Transverse force:')
 	st.latex(latex(F_cr_func(Type)))
 	st.latex(latex(F_cr_func(Type, **db)))
@@ -102,54 +101,57 @@ if db['S_S'] < db['a']:
 	st.latex(latex(F_cr_val)+f'[kN]')
 	db['F_cr']=F_cr_val.rhs
 	
-	
 	if db['F_cr'] < db['F_Ed']:
-		st.write("$F_{cr}< F_{Ed}$,")
-		st.write('FIN! La estructura no cumple frente a patch loading')
-	
-	st.latex(latex(m_1_func(Type)))
-	st.latex(latex(m_1_func(Type, **db)))
-	m_1_val=N(m_1_func(Type, **db). doit(),4)
-	st.latex(latex(m_1_val)+f'[m]')
-	db['m_1']=m_1_val.rhs
-	
-	st.latex(latex(m_2_func(Type)))
-	st.latex(latex(m_2_func(Type, **db)))
-	m_2_val=N(m_2_func(Type, **db). doit(),4)
-	st.latex(latex(m_2_val)+f'[m]')
-	db['m_2']=m_2_val.rhs
-	
-	if Type=="C":
-		st.latex(latex(l_e_func(Type)))
-		l_e_val=N(l_e_func(Type, **db). doit(),4)
-		st.latex(latex(l_e_val)+f'[m]')
-		db['l_e']=l_e_val.rhs # check the conditions
-	st.write("Effective loaded length $l_y$")	
-	st.latex(latex(l_y_func(Type)))
-	
-	l_y_val=N(l_y_func(Type, **db). doit(),4)
-	
-	
-	if Type=="A" or Type=="B": 
-		if l_y_val.rhs>db['a']:
-			st.write("but l y ≤ distance between adjacent transverse stiffeners")
-			st.write(f'$l_y=$ {db["a"]} [m]')
-			#l_y_val.rhs=db['a']
-			db['l_y']=db['a']
-			#st.latex(latex(Eq[l_y,db['a']])+f'[m]')
-	st.latex(latex(l_y_val)+f'[m]')
-	db['l_y']=l_y_val.rhs
-	
-	st.latex(latex(F_y_func(Type)))
-	st.latex(latex(F_y_func(Type, **db)))
-	F_y_val=N(F_y_func(Type, **db). doit(),4)
-	st.latex(latex(F_y_val)+f'[kN]')
-	db['F_y']=F_y_val.rhs
-	
-	if db['F_y'] < db['F_Ed']:
-		st.write('$F_y < F_{Ed}$ FIN! La estructura no cumple frente a patch loading')
-	
+		st.markdown("""$F_{cr}< F_{Ed}$,""")
+		st.markdown("""FIN! La estructura no cumple frente a patch loading""")
 	else:
+		st.markdown("""Calculate $m_1$""")
+		st.latex(latex(m_1_func(Type)))
+		st.latex(latex(m_1_func(Type, **db)))
+		m_1_val=N(m_1_func(Type, **db). doit(),4)
+		st.latex(latex(m_1_val)+f'[m]')
+		db['m_1']=m_1_val.rhs
+		
+		st.markdown("""Calculate $m_2$""")
+		st.latex(latex(m_2_func(Type)))
+		st.latex(latex(m_2_func(Type, **db)))
+		m_2_val=N(m_2_func(Type, **db). doit(),4)
+		st.latex(latex(m_2_val)+f'[m]')
+		db['m_2']=m_2_val.rhs
+	
+		if Type=="C":
+			st.latex(latex(l_e_func(Type)))
+			l_e_val=N(l_e_func(Type, **db). doit(),4)
+			st.latex(latex(l_e_val)+f'[m]')
+			db['l_e']=l_e_val.rhs # check the conditions
+			
+		st.markdown("""Effective loaded length $l_y$""")	
+		st.latex(latex(l_y_func(Type)))
+		
+		l_y_val=N(l_y_func(Type, **db). doit(),4)
+	
+	
+		if Type=="A" or Type=="B": 
+			if l_y_val.rhs>db['a']:
+				st.write("but l y ≤ distance between adjacent transverse stiffeners")
+				st.write(f'$l_y=$ {db["a"]} [m]')
+				#l_y_val.rhs=db['a']
+				db['l_y']=db['a']
+				#st.latex(latex(Eq[l_y,db['a']])+f'[m]')
+		st.latex(latex(l_y_val)+f'[m]')
+		db['l_y']=l_y_val.rhs
+		
+		st.markdown("""Calculate $F_y$""")
+		st.latex(latex(F_y_func(Type)))
+		st.latex(latex(F_y_func(Type, **db)))
+		F_y_val=N(F_y_func(Type, **db). doit(),4)
+		st.latex(latex(F_y_val)+f'[kN]')
+		db['F_y']=F_y_val.rhs
+	
+		if db['F_y'] < db['F_Ed']:
+			st.markdown("""$F_y < F_{Ed}$ FIN! La estructura no cumple frente a patch loading""")
+	
+		st.markdown("""Calculate $\lambda_F$""")
 		st.latex(latex(lambda_F_func(Type)))
 		st.latex(latex(lambda_F_func(Type, **db)))
 		lambda_F_val=N(lambda_F_func(Type, **db). doit(),4)
@@ -159,12 +161,13 @@ if db['S_S'] < db['a']:
 		
 	
 		if db['lambda_F']<=0.5:
-			st.write("The value of $\lambda_F \leq 0.5$ then")
+			st.markdown("""The value of $\lambda_F=${db['lambda_F']} $< 0.5$ then we need to recalculate $m_2=0$""")
 			del db['m_2']
 			if 'l_e' in db.keys():
 				del db['l_e']
 			del db['l_y']
 			del db['F_y']
+			del db['lambda_F']
 			
 
 			st.latex(latex(Eq(m_2,0)))
@@ -175,7 +178,7 @@ if db['S_S'] < db['a']:
 				l_e_val=N(l_e_func(Type, **db). doit(),4)
 				st.latex(latex(l_e_val)+f'[m]')
 				db['l_e']=l_e_val.rhs
-			st.write("$l_y$ is the effective loaded length")
+			st.markdown("""$l_y$ is the effective loaded length""")
 			st.latex(latex(l_y_func(Type)))
 			st.latex(latex(l_y_func(Type, **db)))
 			l_y_val=N(l_y_func(Type, **db). doit(),4)
@@ -189,16 +192,18 @@ if db['S_S'] < db['a']:
 			db['F_y']=F_y_val.rhs
 		
 			if db['F_y'] < db['F_Ed']:
-				st.write('$F_y < F_{Ed}$ FIN! La estructura no cumple frente a patch loading')
+				st.markdown("""$F_y < F_{Ed}$ FIN! La estructura no cumple frente a patch loading""")
+			
+			st.latex(latex(lambda_F_func(Type)))
+			st.latex(latex(lambda_F_func(Type, **db)))
+			lambda_F_val=N(lambda_F_func(Type, **db). doit(),4)
+			st.latex(latex(lambda_F_val))
+			db['lambda_F']=lambda_F_val.rhs
+			
+			if db['lambda_F']<=0.5:
+				st.markdown(f""" $m_2=0$,  $\lambda_F=${db['lambda_F']} $< 0.5$, does not satisfy the condition""")
 			
 			else:
-				del db['lambda_F']
-				st.latex(latex(lambda_F_func(Type)))
-				st.latex(latex(lambda_F_func(Type, **db)))
-				lambda_F_val=N(lambda_F_func(Type, **db). doit(),4)
-				st.latex(latex(lambda_F_val))
-				db['lambda_F']=lambda_F_val.rhs
-				
 				st.write("Reduction factor $\chi_F$ for effective length for resistance")
 				st.latex(latex(chi_F_func(Type)))
 				st.latex(latex(chi_F_func(Type, **db)))
@@ -211,16 +216,18 @@ if db['S_S'] < db['a']:
 				F_Rd_val=N(F_Rd_func(Type, **db). doit(),4)
 				st.latex(latex(F_Rd_val))
 				db['F_Rd']=F_Rd_val.rhs
-			
+		
 				if db['F_Rd']<db['F_Ed']:
 					st.write("$F_{Rd}< F_{Ed}$,")
 					st.write('FIN! La estructura no cumple frente a patch loading')
+					
+				else:
 
-				st.latex(latex(eta_2_func(Type)))
-				st.latex(latex(eta_2_func(Type, **db)))
-				eta_2_val=N(eta_2_func(Type, **db). doit(),4)
-				st.latex(latex(eta_2_val))
-				db['eta_2']=eta_2_val.rhs
+					st.latex(latex(eta_2_func(Type)))
+					st.latex(latex(eta_2_func(Type, **db)))
+					eta_2_val=N(eta_2_func(Type, **db). doit(),4)
+					st.latex(latex(eta_2_val))
+					db['eta_2']=eta_2_val.rhs
 		else:
 			st.write("Reduction factor $\chi_F$ for effective length for resistance")
 			st.latex(latex(chi_F_func(Type)))
@@ -242,7 +249,9 @@ if db['S_S'] < db['a']:
 			
 			st.write("The verification should be performed as follows:")
 			st.latex(latex(eta_2_func(Type)))
-			st.latex(latex(eta_2_func(Type, **db)))
+			#latex_expression=r"latex(eta_2_func(Type,**db))"
+			latex_expression=latex(eta_2_func(Type,**db))
+			st.latex(latex_expression)
 			eta_2_val=N(eta_2_func(Type, **db). doit(),4)
 			st.latex(latex(eta_2_val))
 			db['eta_2']=eta_2_val.rhs
