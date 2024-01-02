8f5c766826c30e1cbb8651f295c0ec019ac75124
NamNguyen2015 <111955400+NamNguyen2015@users.noreply.github.com>
2024-01-02 14:59:38 +0100

    fixed

--- st_Patch_Loading.py
+++ st_Patch_Loading.py
@@ -52,29 +52,29 @@ st.markdown('**6.2 Input datas**')
 # Input parameters
 
 #
-F_Ed_val = st.sidebar.number_input('Design transverse force $F_{Ed} [kN]$', value= 1415.0, min_value=100.0, step=10.0, format="%.1f")
+F_Ed_val = st.sidebar.number_input('$F_{Ed} [kN]$-Fuerza de cálculo/ Design transverse force ', value= 1415.0, min_value=100.0, step=10.0, format="%.1f")
 #
-t_w_val = st.sidebar.number_input('thickness of the plate $t_{w} [m]$', value= 15e-3, min_value=0.0, step=0.10, format="%.3f")
+t_w_val = st.sidebar.number_input('$t_{w} [m]$-Espesor del alma/ Thickness of the plate', value= 15e-3, min_value=0.0, step=0.10, format="%.3f")
 #
-h_w_val = st.sidebar.number_input('clear web depth between flanges $h_w [m]$', value= 270.0e-3, min_value=0.0, step=0.10, format="%.3f")
+h_w_val = st.sidebar.number_input('$h_w [m]$-Altura del ala/ Clear web depth between flanges ', value= 270.0e-3, min_value=0.0, step=0.10, format="%.3f")
 
-b_f_val = st.sidebar.number_input('being taken as not larger than $15\\epsilon*t_f$ on each side of the web $b_{f} [m]$', value= 300.0e-3, min_value=0.0, step=0.10, format="%.3f")
+b_f_val = st.sidebar.number_input('$b_{f} [m]$-Ancho del ala/ Flange width ', value= 300.0e-3, min_value=0.0, step=0.10, format="%.3f")
 
-f_yf_val = st.sidebar.number_input(' the yield strength of the web $f_{yf} [kN/m^2]$', value= 355.0e3, min_value=0.10, step=0.0, format="%.3f")
+f_yf_val = st.sidebar.number_input('$f_{yf} [kN/m^2]$-Resistencia del ala/ The yield strength of the web', value= 355.0e3, min_value=0.10, step=0.0, format="%.3f")
 
-t_f_val = st.sidebar.number_input('Design transverse force $F_{ED} [m]$', value= 30.0e-3, min_value=0.0, step=0.10, format="%.3f")
+t_f_val = st.sidebar.number_input('$t_{f} [m]$-Espesor del ala superior/ Flange thickness', value= 30.0e-3, min_value=0.0, step=0.10, format="%.3f")
 
-f_yw_val = st.sidebar.number_input(' the yield strength of the web $f_{yw} [kN/m^2]$', value= 255.0e3, min_value=0.0, step=0.10, format="%.3f")
+f_yw_val = st.sidebar.number_input('$f_{yw} [kN/m^2]$-Resistencia del alma/ The yield strength of the web', value= 255.0e3, min_value=0.0, step=0.10, format="%.3f")
 
-gamma_M1_val = st.sidebar.number_input('Design transverse force $\\gamma_{M1} $', value= 1.1, min_value=0.0, step=0.01, format="%.3f")
+gamma_M1_val = st.sidebar.number_input('$\\gamma_{M1}$-Coefficiente seguridad ($=1.1$ recomendado)/ Security coeficient', value= 1.1, min_value=0.0, step=0.01, format="%.3f")
 
-E_val = st.sidebar.number_input('Elasticity module $E [kN/m^2]$', value= 200.0e6, min_value=1.0e6, step=1.0e6, format="%.1f")
+E_val = st.sidebar.number_input('$E [kN/m^2]$-Elasticity module (recommended 200e6)' , value= 200.0e6, min_value=1.0e6, step=1.0e6, format="%.1f")
 
-s_s_val = st.sidebar.number_input(' $s_s [m]$', value= 300.0e-3, min_value=0.0, step=0.10, format="%.3f")
+s_s_val = st.sidebar.number_input('$s_s [m]$-Longitudinal width of the load ', value= 300.0e-3, min_value=0.0, step=0.10, format="%.3f")
 
-a_val = st.sidebar.number_input('length of a stiffened or unstiffened plate $a [m]$', value= 1185.0e-3, min_value=0.0, step=0.10, format="%.3f")
+a_val = st.sidebar.number_input('$a [m]$-Distancia entre rigidizadores/ Length of a stiffened or unstiffened plate', value= 1185.0e-3, min_value=0.0, step=0.10, format="%.3f")
 
-c_val = st.sidebar.number_input('Distance $c [m]$', value= 900e-3, min_value=0.0, step=0.10, format="%.3f")
+c_val = st.sidebar.number_input('$c [m]$-Distancia (utilized exclusively when type C presented)', value= 900e-3, min_value=0.0, step=0.10, format="%.3f")
 
 db={'F_Ed':F_Ed_val, 't_w': t_w_val, 'h_w': h_w_val, 'b_f':b_f_val,
 	"f_yf": f_yf_val, "t_f":t_f_val, "f_yw":f_yw_val, "gamma_M1": gamma_M1_val,
@@ -86,14 +86,18 @@ Types=["A","B","C"]
 Type= st.selectbox("chose type of load applicatrion", options=Types)
 
 if db['s_s'] < db['a']:
-	st.write("The interaction of the transverse force, bending moment and axial force should be verified using 7.2.")
+	st.write('Calculation if Buckling coefficients using Figure 6.1 "Buckling coefficients for different types of load application from articale 6.1 basis"')
 	st.latex(latex(k_F_func(Type)))
 	st.latex(latex(k_F_func(Type, **db)))
 	k_F_val=N(k_F_func(Type, **db). doit(),4)
 	st.latex(latex(k_F_val))
-	db['k_F']=k_F_val.rhs
 	
-	st.write('Transverse force:')
+	if Type=="C" and k_F_val.rhs>6.0:
+		st.markdown("""$k_F> 6.0$ in this case, then $k_F$ will take the value equal $6.0$""")
+		db['k_F']=6.0	
+	else:
+		db['k_F']=k_F_val.rhs
+	st.write('Calculation of Critical force using expression (6.5) from artical 6.4 "Reduction factor $\chi_F$ for efective length for resistance"')
 	st.latex(latex(F_cr_func(Type)))
 	st.latex(latex(F_cr_func(Type, **db)))
 	F_cr_val=N(F_cr_func(Type, **db). doit(),4)
@@ -102,16 +106,16 @@ if db['s_s'] < db['a']:
 	
 	if db['F_cr'] < db['F_Ed']:
 		st.markdown("""$F_{cr}< F_{Ed}$,""")
-		st.markdown("""FIN! La estructura no cumple frente a patch loading""")
+		st.markdown("""FIN! La estructura no cumple frente a patch loading  por mucho. Redimensionar a perfiles mucho mayores""")
 	else:
-		st.markdown("""Calculate $m_1$""")
+		st.markdown("""Using expression (6.8) from article 6.5 "Effective loaded length", calculate $m_1$""")
 		st.latex(latex(m_1_func(Type)))
 		st.latex(latex(m_1_func(Type, **db)))
 		m_1_val=N(m_1_func(Type, **db). doit(),4)
 		st.latex(latex(m_1_val)+f'[m]')
 		db['m_1']=m_1_val.rhs
 		
-		st.markdown("""Calculate $m_2$""")
+		st.markdown("""Using expression (6.9) from article 6.5 "Effective loaded length", calculate $m_2$""")
 		st.latex(latex(m_2_func(Type)))
 		st.latex(latex(m_2_func(Type, **db)))
 		m_2_val=N(m_2_func(Type, **db). doit(),4)
@@ -120,25 +124,28 @@ if db['s_s'] < db['a']:
 	
 		if Type=="C":
 			st.latex(latex(l_e_func(Type)))
+			st.latex(latex(l_e_func(Type, **db)))
 			l_e_val=N(l_e_func(Type, **db). doit(),4)
 			st.latex(latex(l_e_val)+f'[m]')
 			db['l_e']=l_e_val.rhs # check the conditions
 			
-		st.markdown("""Effective loaded length $l_y$""")	
+		st.markdown("""Using expression (6.10) from article 6.5 "Effective loaded length", calculate $l_y$""")	
 		st.latex(latex(l_y_func(Type)))
 		l_y_val=N(l_y_func(Type, **db). doit(),4)
+		st.latex(latex(l_y_val)+f'[m]')
 	
 		if Type=="A" or Type=="B": 
+			if l_y_val.rhs<=db['a']:
+				st.write(f'$l_y=${l_y_val.rhs} $<$ $a=$ {db["a"]} satisfies the condition $l_y<a$')
 			if l_y_val.rhs>db['a']:
-				st.write("but l y ≤ distance between adjacent transverse stiffeners")
+				st.write(f'The result $l_y=${l_y_val.rhs} $>$ $a=$ {db["a"]}, due to $l_y$ must be smaller than the distance between adjacent transverse stiffeners $a$, so in this case $l_y$ will take the value equal to $a$={db["a"]}')
 				st.write(f'$l_y=$ {db["a"]} [m]')
-				#l_y_val.rhs=db['a']
 				db['l_y']=db['a']
-				#st.latex(latex(Eq[l_y,db['a']])+f'[m]')
-		st.latex(latex(l_y_val)+f'[m]')
+				
+		
 		db['l_y']=l_y_val.rhs
 		
-		st.markdown("""Calculate $F_y$""")
+		st.markdown("""Calculation of plastic web yielding mechanism $F_y$""")
 		st.latex(latex(F_y_func(Type)))
 		st.latex(latex(F_y_func(Type, **db)))
 		F_y_val=N(F_y_func(Type, **db). doit(),4)
@@ -146,17 +153,17 @@ if db['s_s'] < db['a']:
 		db['F_y']=F_y_val.rhs
 	
 		if db['F_y'] < db['F_Ed']:
-			st.markdown("""$F_y < F_{Ed}$ FIN! La estructura no cumple frente a patch loading""")
+			st.markdown("""$F_y < F_{Ed}$ FIN! La estructura no cumple frente a patch loading. Redimensionar a perfiles mayores o acercar rigidizadores""")
 	
-		st.markdown("""Calculate $\lambda_F$""")
+		st.markdown("""$\lambda_F$, non dimensional Slenderness using modification of expression (6.4) from article 6.4 "Reduction factor $\chi_F$ for efective lenght for resistance" """)
 		st.latex(latex(lambda_F_func(Type)))
-		st.latex(latex(lambda_F_func1(Type, **db)))
+		#st.latex(latex(lambda_F_func1(Type, **db)))
 		lambda_F_val=N(lambda_F_func(Type, **db). doit(),4)
 		st.latex(latex(lambda_F_val))
 		db['lambda_F']=lambda_F_val.rhs
 
 		if db['lambda_F']<=0.5:
-			st.markdown("""The value of $\lambda_F=${db['lambda_F']} $< 0.5$ then we need to recalculate $m_2=0$""")
+			st.write(f'The value of $\lambda_F=$ {db["lambda_F"]} $< 0.5$ then we need to recalculate $m_2=0$')
 			del db['m_2']
 			if 'l_e' in db.keys():
 				del db['l_e']
@@ -173,7 +180,7 @@ if db['s_s'] < db['a']:
 				db['l_e']=l_e_val.rhs
 			st.markdown("""$l_y$ is the effective loaded length""")
 			st.latex(latex(l_y_func(Type)))
-			st.latex(latex(l_y_func(Type, **db)))
+			#st.latex(latex(l_y_func(Type, **db)))
 			l_y_val=N(l_y_func(Type, **db). doit(),4)
 			st.latex(latex(l_y_val)+f'[m]')
 			db['l_y']=l_y_val.rhs
@@ -211,8 +218,9 @@ if db['s_s'] < db['a']:
 				db['F_Rd']=F_Rd_val.rhs
 		
 				if db['F_Rd']<db['F_Ed']:
-					st.write("$F_{Rd}< F_{Ed}$,")
-					st.write('FIN! La estructura no cumple frente a patch loading')
+					st.markdown("""$F_y < F_{Ed}$ FIN! La estructura no cumple frente a patch loading. Redimensionar a perfiles mayores o acercar rigidizadores""")
+					#st.write("$F_{Rd}< F_{Ed}$,")
+					#st.write('FIN! La estructura no cumple frente a patch loading')
 				else:
 					st.latex(latex(eta_2_func(Type)))
 					st.latex(latex(eta_2_func(Type, **db)))
@@ -236,20 +244,25 @@ if db['s_s'] < db['a']:
 			if db['F_Rd']<db['F_Ed']:
 				st.write("$F_{Rd}< F_{Ed}$,")
 				
-				st.write('FIN! La estructura no cumple frente a patch loading')
+				st.markdown("""FIN! La estructura no cumple frente a patch loading. Redimensionar a perfiles mayores o acercar rigidizadores""")
 			
-			st.write("The verification should be performed as follows:")
+			st.write("La sección cumple frente a patch loading con un coeficiente de seguridad de")
 			st.latex(latex(eta_2_func(Type)))
 
-			
 			#st.latex(latex(eta_2_func1(Type, **db)))
 			eta_2_val=N(eta_2_func(Type, **db). doit(),4)
 			st.latex(latex(eta_2_val))
 			db['eta_2']=eta_2_val.rhs
+			# Load an image from file
+			image = Image.open("eta_1.png")
+			# Display the image
+			st.image(image, width=550)
+			
+			
 			
 else:
 	sst.write("$S_s> a$,")
-	st.write('FIN! La estructura no cumple frente a patch loading')
+	st.write('FIN! Este cálculo no se puede hacer mediante patch loading. Realizar como si fuera una columna comprimidad con sus reducciones y posibles pandeos')
 
 
 st.markdown('---')
